fa4a25c8107f0e95de1ba4958bfad83fdf1134e9
sumine <alejandra.ppacheco@gmail.com>
2016-08-02 11:20:50 -0500

    Agent's Proyect
    Proyecto de Agentes

--- CameraAgent.java
+++ CameraAgent.java
@@ -1,4 +1,7 @@
-
+//C:\Users\alejandra\Downloads\JADE-all-4.4.0\JADE-bin-4.4.0\jade>javac    ----   este es el directoria donde tienes jadex!!
+// javac -cp jade.jar behaviours/CallForPosition.java --- compilar
+ //java -cp jade.jar;. jade.Boot -agents agent1:CameraAgent  -- crear el agente "agent1" de clase "Camera Agent"
+ //javac -cp jade.jar;. CameraAgent.java
 import jade.core.Agent;
 import jade.core.AID;
 import jade.lang.acl.ACLMessage;
@@ -12,7 +15,7 @@ public class CameraAgent extends Agent{
 
 		ACLMessage msg = new ACLMessage(ACLMessage.INFORM); 
 		
-		msg.addReceiver(new AID("Robot1", AID.ISLOCALNAME)); 
+		msg.addReceiver(new AID("agent2", AID.ISLOCALNAME)); 
 		msg.setLanguage("English"); 
 		msg.setOntology("Weather-forecast-ontology"); 
 		msg.setContent("Today it’s raining");
--- RobotAgent.java
+++ RobotAgent.java
@@ -1,10 +1,13 @@
+//C:\Users\alejandra\Downloads\JADE-all-4.4.0\JADE-bin-4.4.0\jade>javac    ----   este es el directoria donde tienes jadex!!
 
+//java -cp jade.jar;. jade.Boot -agents agent2:RobotAgent -gui ----
+ //javac -cp jade.jar;. RobotAgent.java
 import jade.core.Agent;
 import jade.lang.acl.ACLMessage;
 import java.util.Iterator;
 import behaviours.CallForPosition;
 
-//javac -cp jade.jar behaviours/MoveRobotBehaviour.java
+//javac -cp jade.jar behaviours/CallForPosition.java
 //java -cp jade.jar:. jade.Boot -agents Luis:HelloWorAgent -gui
 //java -cp jade.jar:. jade.Boot -agents "Luis2:HelloWldAgent(1 k arg3)"
 
@@ -13,7 +16,7 @@ public class RobotAgent extends Agent {
 	private CallForPosition moveBehaviour;
 
 	protected void setup() { 
-		System.out.println("I’m a robot agent!"); 
+		System.out.println("Im a robot agent!"); 
 		addBehaviour(new CallForPosition());
     }
 
