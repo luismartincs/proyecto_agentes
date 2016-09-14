0ba8cc9ef9031f8c3263a95eddb0aa90dcc2ea26
Luis Martin <luismartin.925@gmail.com>
2016-09-14 00:53:20 -0500

    Plan object

--- jaya/PlanObject.java
+++ jaya/PlanObject.java
@@ -30,4 +30,39 @@ public class PlanObject{
 	public ArrayList<int[]> getPoints(){
 		return this.points;
 	}
+
+    public static PlanObject getFromString(String protocol){       
+               
+         PlanObject plan = new PlanObject();       
+               
+         ArrayList<int[]> points = new ArrayList<>();      
+         String objects[] = protocol.split(";");       
+               
+         for (int i = 0; i < objects.length; i++) {        
+             if(i==0){     
+                 plan.setTime(Integer.parseInt(objects[i]));       
+             }else{        
+                 String xy[] = objects[i].split(",");      
+                 points.add(new int[]{Integer.parseInt(xy[0]),Integer.parseInt(xy[1])});       
+             }     
+         }     
+               
+         plan.setPoints(points);       
+               
+         return plan;      
+     }     
+       
+     @Override     
+     public String toString() {        
+               
+         StringBuilder str =  new StringBuilder();     
+               
+         str.append(time);     
+         for (int i = 0; i < points.size(); i++) {     
+             int point[] = points.get(i);                  
+             str.append(";"+point[0]+","+point[1]);        
+         }     
+               
+         return str.toString();        
+     }
 }
